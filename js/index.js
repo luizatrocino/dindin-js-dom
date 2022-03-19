7e5ae02b943ce48cec07f4809d69db8934ebeb8a
Luiza Trocino <luizatrocino@hotmail.com>
2022-03-19 13:58:11 -0300

    novos ajustes

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-// O header deve ter cor de fundo #2E948A.
+// 1- O header deve ter cor de fundo #2E948A.
 
 let header = document.getElementsByTagName('header')
 
@@ -7,22 +7,21 @@ let header = document.getElementsByTagName('header')
 header[0].style.backgroundColor = '#2E948A'
 
 
-// No menu do header, está faltando o link do item Cursos que deve redirecionar
-// para cursos.html
+// 2- No menu do header, está faltando o link do item Cursos que deve redirecionar para cursos.html
 
 let linkCursos = document.querySelector('#menu_opcoes nav a:nth-child(1)')
 // console.log(linkCursos)
 
 linkCursos.setAttribute('href', 'cursos.html')
 
-// Na etapa de depoimentos, o título deveria ser "O que falam sobre nós".
+// 1 - Na etapa de depoimentos, o título deveria ser "O que falam sobre nós".
 
 let tituloDepo = document.querySelector('.titulo.depoimento h3')
 // console.log(tituloDepo)
 
 tituloDepo.innerHTML = "O que falam sobre nós"
 
-// No banner da home, está faltando centralizar os elementos filhos da section que tem o id #introducao.
+// 2 - No banner da home, está faltando centralizar os elementos filhos da section que tem o id #introducao.
 
 let banner = document.querySelector('#introducao')
 // console.log(banner)
@@ -30,14 +29,14 @@ let banner = document.querySelector('#introducao')
 banner.style.justifyContent = 'center'
 
 
-// Na etapa de blog, o título deveria ser "Mais conteúdo pra você".
+// 3 - Na etapa de blog, o título deveria ser "Mais conteúdo pra você".
 
 let blog = document.querySelectorAll('.titulo h3')
 // console.log(blog[1])
 
 blog[1].innerHTML="Mais conteúdo para você"
 
-// Todos os textos que estiverem com a classe .titulo devem apresentar todas as letras maiúsculas.
+// 4 - Todos os textos que estiverem com a classe .titulo devem apresentar todas as letras maiúsculas.
 
 let maiuscula = document.querySelectorAll('.titulo')
 
@@ -48,7 +47,7 @@ maiuscula.forEach( titulo => {
 })
 
 
-// o botão "ver todos os posts" deve ter um link que direciona para o arquivo blog.html
+// 5 - o botão "ver todos os posts" deve ter um link que direciona para o arquivo blog.html
 
 let botao = document.querySelector('#todos_posts')
 
@@ -57,27 +56,32 @@ let botao = document.querySelector('#todos_posts')
 botao.setAttribute('href','blog.html')
 
 
-// Adicionar um novo curso na section que contém o id investimentos_poupando_independencia:
+// 6- Adicionar um novo curso na section que contém o id investimentos_poupando_independencia:
 
 let cursos = document.querySelector('#investimentos_poupando_independencia')
 
-console.log(cursos)
-
 let novoCurso = document.createElement('div')
+novoCurso.setAttribute('id',"independencia")
 
 cursos.appendChild(novoCurso)
 
-novoCurso.setAttribute('id',"independencia")
-
-console.log(cursos)
-
 novoCurso.appendChild(document.createElement('img'))
+let imagem = document.querySelector('#investimentos_poupando_independencia #independencia img')
+imagem.setAttribute('src','imagens/independencia_financeira.png')
+imagem.setAttribute('width','180px')
+imagem.setAttribute('alt','Independência Financeira')
+
 novoCurso.appendChild(document.createElement('h2'))
+let h2 = document.querySelector('#investimentos_poupando_independencia #independencia h2')
+h2.innerHTML="Independencia Financeira"
+
 novoCurso.appendChild(document.createElement('p'))
+let p = document.querySelector('#investimentos_poupando_independencia #independencia p')
+p.innerHTML="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+
 novoCurso.appendChild(document.createElement('a'))
-
-document.querySelector('#investimentos_poupando_independencia #independencia img').setAttribute('src','imagens/independencia_financeira.png')
-
-console.log(cursos)
-
+let a = document.querySelector('#investimentos_poupando_independencia #independencia a')
+a.setAttribute('class','comecar_agora')
+a.setAttribute('href','curso.html')
+a.innerHTML="começar agora"
 
